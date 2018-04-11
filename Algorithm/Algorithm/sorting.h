7071bc7d3bfb37291a8028658ee44193def46ec6
pohan lee <windvergil@gmail.com>
2018-04-11 17:19:15 +0800

    Removed the sorting into Sorting class

--- Algorithm/Algorithm/sorting.h
+++ Algorithm/Algorithm/sorting.h
@@ -1,8 +1,14 @@
 #pragma once
-class sorting
-{
-public:
-	sorting();
-	~sorting();
-};
+#include <vector>
 
+void shellSort(std::vector<int> &);
+void insertionSort(std::vector<int> &);
+void mergeSort(std::vector<int> &, int, int);
+void merge(std::vector<int> &, int, int, int);
+int min(int, int);
+void mergeSort_iteration(std::vector<int> & );
+void selectionSort(std::vector<int> &);
+void bubbleSort(std::vector<int> &);
+void quickSort(std::vector<int> &, int, int);
+int partition(std::vector<int>&, int, int);
+void quickSort2(std::vector<int>&, int , int );
